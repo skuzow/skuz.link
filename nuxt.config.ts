840b14f6e5ec723a37f631ac2024ab3d7574dbc1
skuzow <alex@skuzow.net>
2025-12-11 23:58:11 +0100

    build: deploy cloudflare worker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,20 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', 'shadcn-nuxt', 'nitro-cloudflare-dev'],
 
   css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [tailwindcss()]
   },
 
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  nitro: {
+    preset: 'cloudflare_module',
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  }
 });
