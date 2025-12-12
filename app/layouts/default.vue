f3ac74dbf9030221a4cdfe09b21d8d3b8a3b8b26
skuzow <alex@skuzow.net>
2025-12-12 22:15:10 +0100

    fix(layouts): main x padding

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -59,7 +59,7 @@ useSeoMeta({
 <template>
   <div class="relative grid min-h-dvh grid-rows-[auto_1fr_auto] gap-2">
     <!-- <NavHeader /> -->
-    <main class="container mb-4 max-w-6xl py-4 md:my-11 md:px-8">
+    <main class="mb-4 max-w-6xl p-4 md:my-11 md:px-8">
       <slot />
     </main>
     <!-- <NavFooter /> -->
