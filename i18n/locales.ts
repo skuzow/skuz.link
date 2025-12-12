0d027accda3ab43ecb6d1dcb98783a5032812ee2
skuzow <alex@skuzow.net>
2025-12-12 21:06:24 +0100

    feat: i18n internationalization

---
+++ i18n/locales.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    code: 'en',
+    name: 'English',
+    file: 'en-US.json'
+  },
+  {
+    code: 'es',
+    name: 'Español',
+    file: 'es-ES.json'
+  }
+];
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,26 @@
 import tailwindcss from '@tailwindcss/vite';
+import locales from './i18n/locales';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'shadcn-nuxt', 'nitro-cloudflare-dev'],
 
   css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/i18n',
+    'shadcn-nuxt',
+    'nitro-cloudflare-dev'
+  ],
+
+  i18n: {
+    defaultLocale: 'en',
+    locales
   },
 
   nitro: {
