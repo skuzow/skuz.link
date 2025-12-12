3364d20ce533e0ec4225da0458dfb974d3f4e2ec
skuzow <alex@skuzow.net>
2025-12-12 01:14:38 +0100

    feat: default geist font with preload

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,19 @@
+<script lang="ts" setup>
+if (import.meta.server) {
+  useHead({
+    link: [
+      {
+        rel: 'preload',
+        href: '/fonts/Geist[wght].woff2',
+        as: 'font',
+        type: 'font/woff2',
+        crossorigin: ''
+      }
+    ]
+  });
+}
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <h1 class="text-5xl font-bold">Hello world</h1>
