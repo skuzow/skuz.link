dfbee147448ab01f2531770bb1aa07e9f524cce2
skuzow <alex@skuzow.net>
2025-12-14 18:40:22 +0100

    feat(footer): logo instead of text

---
+++ app/components/nav/Logo.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+interface Props {
+  size?: number;
+}
+
+const { size = 24 } = defineProps<Props>();
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    :width="size"
+    :height="size"
+    fill="none"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="stroke-foreground"
+  >
+    <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+    <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+  </svg>
+</template>
--- app/components/nav/footer/index.vue
+++ app/components/nav/footer/index.vue
@@ -11,7 +11,10 @@ const currentYear: number = new Date().getFullYear();
     <nav
       class="mx-auto flex w-full max-w-6xl flex-1 flex-col items-center justify-between gap-y-4 px-4 md:flex-row md:gap-y-0 md:px-8"
     >
-      <p class="font-bold">skuz.link / {{ currentYear }}</p>
+      <div class="flex items-center justify-center gap-x-2">
+        <NavLogo :size="20" />
+        <span class="font-bold">/ {{ currentYear }}</span>
+      </div>
 
       <section class="flex items-center gap-x-1 md:gap-x-4">
         <NuxtLink
