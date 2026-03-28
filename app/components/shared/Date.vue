a9e285027a09f6c5a184867d9e25c8f8169ad78d
skuzow <alex@skuzow.net>
2026-03-28 14:14:22 +0100

    feat(shared): date component

---
+++ app/components/shared/Date.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+interface Props {
+  date: Date | string;
+}
+
+const { date } = defineProps<Props>();
+
+const { d } = useI18n();
+</script>
+
+<template>
+  <time :datetime="date.toString()">{{ d(date) }}</time>
+</template>
