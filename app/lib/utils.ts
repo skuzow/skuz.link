7fe1d273957057d02936e604e2f915b19a5221e2
skuzow <alex@skuzow.net>
2026-03-30 23:39:50 +0200

    feat(utils): titlecase

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -9,3 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export const abbreviate = (value: string): string => {
   return value.slice(0, 2).toLocaleUpperCase();
 };
+
+export const titleCase = (value: string): string => {
+  return value[0]?.toUpperCase() + value.substring(1).toLowerCase();
+};
